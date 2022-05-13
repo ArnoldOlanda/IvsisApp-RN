5df349fd5c2455b7b97d728c86cec9ea86ad8a4f
unknown <Jeremy HC>
2022-05-12 23:01:01 -0500

    Se corrigieron algunos errores de los Grupos

--- src/navigator/GroupsStack.js
+++ src/navigator/GroupsStack.js
@@ -1,5 +1,5 @@
 //@ts-check
-/*import React from 'react';
+import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { GroupListScreen } from '../screens/Groups/GroupListScreen';
 import { CreateGroupTabs } from './CreateGroupTabs';
@@ -23,37 +23,6 @@ export const GroupsStack = () => {
       <Stack.Screen name="GroupListScreen" component={ GroupListScreen } />
       <Stack.Screen name="CreateGroupTabs" component={ CreateGroupTabs } />
 
-      {/* <Stack.Screen name="CreateGroupTabs" component={ CreateGroupTabs } /> }
-    </Stack.Navigator>
-  );
-}*/
-
-//@ts-check
-import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
-import { GroupListScreen } from '../screens/Groups/GroupListScreen';
-import { CreateGroupTabs } from './CreateGroupTabs';
-import { NewGroupScreen } from '../screens/Groups/NewGroupScreen';
-import { ContactsListScreen } from '../screens/Groups/ContactsListScreen';
-
-
-const Stack = createStackNavigator();
-
-export const GroupsStack = () => {
-  return (
-    <Stack.Navigator
-      screenOptions={{
-        headerStyle :{
-          elevation : 0,
-          //shadowColor : 'transparent' --opcion solo disponible para ios
-        },
-        cardStyle : { backgroundColor : 'white' },
-        headerShown:false //Oculta el header del stack
-      }}
-    >
-      <Stack.Screen name="ContactsListScreen" component={ ContactsListScreen } />
-
-      {/* <Stack.Screen name="CreateGroupTabs" component={ CreateGroupTabs } /> */}
     </Stack.Navigator>
   );
 }
